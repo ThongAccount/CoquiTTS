4628480a91602f66e4786649c0387988c78bd960
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-06 14:54:28 +0700

    Update main.py

--- coqui-tts-render/main.py
+++ coqui-tts-render/main.py
@@ -18,4 +18,4 @@ def speak():
 
 if __name__ == "__main__":
     os.makedirs("static", exist_ok=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 18000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
